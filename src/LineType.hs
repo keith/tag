9788955ce00b9fefc6a66fb036f9d82947cdf6b4
Keith Smiley <keithbsmiley@gmail.com>
2017-10-03 23:29:05 -0700

    Move LineType regex to function

--- src/LineType.hs
+++ src/LineType.hs
@@ -25,7 +25,7 @@ getLocation line =
   if length matches == 2 && length indexes == 2
     then Just $ Location (Line $ head indexes) (Column $ indexes !! 1)
     else Nothing
-  where matches = getCaptureGroups line "^(?:\x1b\\[[^m]+m)?(\\d+)(?:\x1b\\[0m\x1b\\[K)?:(\\d+):"
+  where matches = getCaptureGroups line searchMatchRegex
         indexes = mapMaybe toNum matches
 
 getFilePath :: String -> Maybe LineType
@@ -33,9 +33,21 @@ getFilePath line =
   case listToMaybe matches of
     Just path -> Just $ FilePath path
     Nothing -> Nothing
-  where matches = getCaptureGroups line "^(?:\x1b\\[[^m]+m)?([^\x1b]+)(?:\x1b\\[0m\x1b\\[K)?"
+  where matches = getCaptureGroups line "^(?:(?:\x1b\\[[^m]*m)+)?([^\x1b]+)"
 
 toNum :: String -> Maybe Int
 toNum string = if all isDigit string
   then Just $ read string
   else Nothing
+
+-- The regex used to extract line and column number from searches
+-- This regex has to handle colors so that we can easily re-output colors to
+-- the UI without processing all strings to remove ANSI escape stuff
+searchMatchRegex :: String
+searchMatchRegex =
+  "^(?:(?:\x1b\\[[^m]*m)+)?\
+  \(\\d+)\
+  \(?:(?:\x1b\\[0?m(?:\x1b\\[K)?)+)?\
+  \:(?:\x1b\\[m)?\
+  \(\\d+)\
+  \(?:\x1b\\[m)?:"
