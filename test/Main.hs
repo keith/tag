50b4cda56bc2cf813e5b3940541ca480a43d96d8
Keith Smiley <keithbsmiley@gmail.com>
2017-10-04 22:25:21 -0700

    Exit with 1 when tests fail
    I didn't realize this was something I needed to do manually

--- test/Main.hs
+++ test/Main.hs
@@ -2,10 +2,13 @@ module Main where
 
 import AgTests
 import RgTests
+import System.Exit
 import Test.HUnit
 
-main :: IO Counts
+main :: IO ()
 main = do
   agTests <- allAgTests
   rgTests <- allRgTests
-  runTestTT $ TestList $ agTests ++ rgTests
+  (Counts _ _ errorCount failureCount) <- runTestTT $
+    TestList $ agTests ++ rgTests
+  if errorCount > 0 || failureCount > 0 then exitFailure else exitSuccess
