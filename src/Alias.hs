6fff820e9049e45bc352ced7f084a3a1146d3390
Keith Smiley <keithbsmiley@gmail.com>
2021-11-14 20:05:26 -0800

    Remove haskell

--- src/Alias.hs
+++
@@ -1,9 +0,0 @@
-module Alias where
-
-aliasForCommand :: Int -> String -> String
-aliasForCommand index string =
-  "alias e" ++ show index ++ "=\"" ++ string ++ "\""
-
-globalAliasForCommand :: Int -> String -> String
-globalAliasForCommand index string =
-  "alias -g f" ++ show index ++ "=\"'" ++ string ++ "'\""
--- src/EitherUtils.hs
+++
@@ -1,5 +0,0 @@
-module EitherUtils where
-
-maybeToEither :: e -> Maybe a -> Either e a
-maybeToEither _ (Just a) = Right a
-maybeToEither e _ = Left e
